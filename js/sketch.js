c75c947b4a0b43d1685fd3e1feec5a206ccdc7da
Toriyuki <51471099+Toriyuki@users.noreply.github.com>
2022-03-08 21:16:57 -0600

    Updated Sounds

--- js/sketch.js
+++ js/sketch.js
@@ -1,7 +1,70 @@
+let doorClosed, doorOpened;
+let doorState = "CLOSED";
+let doorCloseSound = new Tone.MonoSynth(
+  {
+    "modulationEnvelope": {
+      "attack": 0.01,
+      "decay": 0,
+      "sustain": 0,
+      "release": 1
+    }
+});
+
+let doorCloseSoundEffect = new Tone.AutoPanner(
+  {
+    "frequency": "8n",
+    "type": "square6",
+    "depth": 0,
+    "wet": 0.5
+}).toDestination();
+
+let doorOpenSound = new Tone.AMSynth(
+  {
+    "envelope": {
+      "attack": 0.006,
+      "decay": 4,
+      "sustain": 0.04,
+      "release": 1.2
+    }
+});
+
+let doorOpenSoundEffect = new Tone.PitchShift(
+  {
+    "pitch": 7,
+    "windowSize": 0.1,
+    "delayTime": 0,
+    "feedback": 0,
+    "wet": 0.5
+  }
+).toDestination();
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 400);
+
+  doorClosed = loadImage("Photos/Door-Close.png");
+  doorOpened = loadImage("Photos/Door-Open.png");
+
+  doorCloseSound.connect(doorCloseSoundEffect);
+  doorOpenSound.connect(doorOpenSoundEffect);
 }
 
 function draw() {
-  background(220);
+  background(255);
+  if(doorState == "CLOSED") {
+    image(doorClosed, 0, 0);
+  }
+  else {
+    image(doorOpened, 0, 0);
+  }
+}
+
+function mousePressed() {
+  if(doorState == "CLOSED") {
+    doorOpenSound.triggerAttackRelease("C5", "8n");
+    doorState = "OPENED";
+  }
+  else {
+    doorCloseSound.triggerAttackRelease("C3", "32n");
+    doorState = "CLOSED";
+  }
 }
